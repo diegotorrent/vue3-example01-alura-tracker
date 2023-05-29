182f0a30d6f2b1cfdbd538afe505b71d2dfe481a
Diego Torrent <diego.torrent@hotmail.com>
2023-05-29 15:59:53 -0300

    Update Formulario.vue

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -7,15 +7,15 @@
             <div class="column">
                 <div class="is-flex is-align-items-center is-justify-content-space-between">
                     <section>
-                        <strong>00:00:00</strong>
+                        <strong>{{tempoDecorrido}}</strong>
                     </section>
-                    <button class="button">
+                    <button class="button" @click="iniciar">
                         <span class="icon">
                             <i class="fas fa-play"></i>
                         </span>
                         <span>play</span>
                     </button>
-                    <button class="button">
+                    <button class="button" @click="finalizar">
                         <span class="icon">
                             <i class="fas fa-stop"></i>
                         </span>
@@ -30,6 +30,31 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 export default defineComponent({
-    name: 'Formulario-home'
+    name: 'Formulario-home',
+    data(){
+        return {
+            tempoEmSegundos: 0,
+            cronometro:0 
+        }
+    },
+    computed: {
+        tempoDecorrido() : string {
+            return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
+        }
+    },
+    methods: {
+        iniciar(){
+            console.log("iniciar click")
+            this.tempoEmSegundos = 0
+            this.finalizar()
+            this.cronometro = setInterval(() => {
+                this.tempoEmSegundos += 1;
+            },1000)
+        },
+        finalizar(){
+            console.log("finalizar click")
+            clearInterval(this.cronometro)
+        }
+    }
 }) 
 </script>
